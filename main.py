2a3047ef7e8c5eb253d5f80a1da456fbdceadda9
DSeratila <dm.seratila@gmail.com>
2024-02-12 23:47:48 +0200

    hangman

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-#Password Generator Project
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import hangman_stages
+import hangman_words
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+print(hangman_stages.logo)
+lives_left = len(hangman_stages.stages)
 
-#easy way
-result = ''
-for letter in range(1, nr_letters + 1):
-    result += letters[random.randint(0, len(letters) - 1)]
+chosen_word = random.choice(hangman_words.word_list)
+# print(chosen_word)
 
-for letter in range(1, nr_symbols + 1):
-    result += numbers[random.randint(0, len(numbers) - 1)]
+display = list(('_' * len(chosen_word)))
 
-for letter in range(1, nr_numbers + 1):
-    result += symbols[random.randint(0, len(symbols) - 1)]
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter:\n ").lower()
 
-print(result)
-print(''.join(random.sample(result, k=len(result))))
+    if guess in display:
+        print("You've already got this one. Choose smth else")
+        continue
 
+    for position in range(len(chosen_word)):
+        if chosen_word[position] == guess:
+            display[position] = guess
 
+    if guess not in chosen_word:
+        if lives_left > 1:
+            print("The letter is not in the world, you've guessed it wrong. You've lost 1 life")
+        lives_left -= 1
+        print(hangman_stages.stages[lives_left])
+        if lives_left == 0:
+            end_of_game = True
+            print("You Lose")
 
+    print(''.join(display))
+
+    if "_" not in display:
+        end_of_game = True
+        print("You won!")
