686098f21ca2f1338cf1029275d28d9a3f7dffe6
DSeratila <dm.seratila@gmail.com>
2024-02-25 21:58:09 +0200

    day 15 coffee machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import art
-from game_data import data
-import random
+# import random
 import subprocess
 
 
@@ -8,57 +6,102 @@ def clear():
     subprocess.call("clear")
 
 
-def get_next(list_data:list):
-    array_len = len(list_data)
-    index = random.randint(0, array_len - 1)
-    return list_data.pop(index)
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+# resources = {
+#     "water": 0,
+#     "milk": 0,
+#     "coffee": 0,
+# }
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+bank = 0.00
 
 
-def check_input(a, b, bid):
-    if bid.lower() == "a":
-        object_1 = a
-        object_2 = b
+def report():
+    print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\nMoney: ${bank}")
+
+
+def enough_resources(ordered_drink):
+    recipe_ingridients = MENU[ordered_drink]["ingredients"]
+    missing_ingrediends = []
+
+    if ordered_drink in MENU.keys():
+        for ingredient_name in recipe_ingridients:
+            if resources[ingredient_name] < recipe_ingridients[ingredient_name]:
+                missing_ingrediends.append(ingredient_name)
+
+        if len(missing_ingrediends) > 0:
+            return "Sorry there is not enough ingredients: " + ','.join(missing_ingrediends)
+
+    return ""
+
+
+def sum_coins(quarters: int, dimes: int, nickles: int, pennies: int):
+    return quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.1
+
+
+def deduct_resources(ordered_drink):
+    for resource in MENU[ordered_drink]["ingredients"]:
+        resources[resource] = resources[resource] - MENU[ordered_drink]["ingredients"][resource]
+
+
+while True:
+    user_input = input("What would you like? (espresso/latte/cappuccino):")
+    if user_input == "report":
+        report()
+    elif user_input == "off":
+        quit()
+    elif user_input in MENU.keys():
+        missing_ingredients = enough_resources(user_input)
+        if missing_ingredients == "":
+            print("Please insert coins.")
+            quarters = int(input("how many quarters?: "))
+            dimes = int(input("how many dimes?: "))
+            nickles = int(input("how many nickles?: "))
+            pennies = int(input("how many pennies?: "))
+
+            user_money = sum_coins(quarters, dimes, nickles, pennies)
+            change = user_money - MENU[user_input]["cost"]
+
+            if change < 0:
+                print("Sorry that's not enough money. Money refunded.")
+
+            deduct_resources(user_input)
+            print(f"Here's ${change} in change.")
+            print(f"Here is your {user_input}")
+            bank += user_money - change
+        else:
+            print(missing_ingredients)
     else:
-        object_1 = b
-        object_2 = a
-
-    if object_1["follower_count"] == object_2["follower_count"]:
-        return "equal"
-    elif object_1["follower_count"] > object_2["follower_count"]:
-        return "You're right"
-    else:
-        return "Sorry, that's wrong"
-
-
-remains = len(data)
-user_score = 0
-lost = False
-object_a = get_next(data)
-
-while remains > 1 and lost is False:
-    object_b = get_next(data)
-    remains = len(data)
-
-    print(art.logo)
-    print("Compare A: {}, a {}, from {} score {}".format(object_a["name"], object_a["description"], object_a["country"],
-                                                         object_a["follower_count"]))
-    print(art.vs)
-    print("Against B: {}, a {}, from {} score {}".format(object_b["name"], object_b["description"], object_b["country"],
-                                                         object_b["follower_count"]))
-
-    user_choice = input("Who has more followers: A or B?: ")
-
-    result = check_input(object_a, object_b, user_choice)
-
-    if result.lower() == "Sorry, that's wrong".lower():
-        lost = True
-        print("{}. Your score = {}".format(result, user_score))
-    else:
-        user_score += 1
-        print("{}. Your score = {}".format(result, user_score))
-        object_a = object_b
-        object_b = get_next(data)
-        clear()
-
-clear()
+        quit()
 
