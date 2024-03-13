70ee6662ceded8c0b2e0f7c1e120416043310d4d
DSeratila <dm.seratila@gmail.com>
2024-03-14 00:49:35 +0200

    day 18 turtle graphics figures drawing

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-# import random
-import subprocess
-from question_model import Question
-from data import question_data
-from  quiz_brain import QuizBrain
+import turtle
+from turtle import Turtle
+import random
 
+t = Turtle()
+t.shape("turtle")
+t.color('red')
+t.screen.colormode(255)
+t.penup()
+t.left(90)
+t.forward(300)
+t.right(90)
+t.pendown()
 
-def clear():
-    subprocess.call("clear")
+for i in range(3, 21):
+    g = random.randrange(0, 257, 10)
+    b = random.randrange(0, 257, 10)
+    r = random.randrange(0, 257, 10)
+    print(r, g, b)
+    tup = (r, g, b)
+    t.pencolor(tup)
+    angle = 360/i
+    # if angle != 90:
+    t.right(angle)
+    for r in range(0, i):
+        if i % 2 == 0:
+            for _ in range(0, 5):
+                t.forward(10)
+                t.penup()
+                t.forward(4)
+                t.pendown()
+                t.forward(2)
+                t.penup()
+                t.forward(4)
+                t.pendown()
+        else:
+            t.forward(100)
+        if r != i-1:
+            t.right(angle)
 
-
-question_bank = []
-
-for q in question_data:
-    question_bank.append(Question(q["text"], q["answer"]))
-
-
-brain = QuizBrain(question_bank)
-
-while brain.still_has_questions():
-    brain.next_question()
-
-print("You've completed the quiz")
-print(F"Your final score is: {brain.score}/{len(question_bank)}")
+screen = turtle.Screen()
+screen.exitonclick()
 
