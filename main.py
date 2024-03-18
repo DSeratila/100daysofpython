6c1c396c3fc66d7cec142605614348d04c4d0964
DSeratila <dm.seratila@gmail.com>
2024-03-18 23:32:37 +0200

    day 18 end: hirst painting

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import turtle
 from turtle import Turtle
 import random
+import colorgram
+
 
 t = Turtle()
 # t.shape("turtle")
 # t.color('red')
-t.screen.colormode(255)
 
 
 def get_ready():
-    # t.pensize(7)
+    t.pensize(20)
+    t.screen.colormode(255)
     t.speed(0)
     # t.penup()
     # t.left(90)
@@ -24,30 +26,30 @@ def change_color_to_random():
     r = random.randrange(0, 257, 10)
     t.pencolor(r, g, b)
 
+def draw_dot(color: turtle):
+    t.pendown()
+    t.pencolor(color[0], color[1], color[2])
+    t.forward(1)
+    t.penup()
 
-def get_random_direction():
-    directions = [270, 90, 0, 180]
-    return random.choice(directions)
-
-
-def draw_line():
-    change_color_to_random()
-    t.setheading(get_random_direction())
-    t.forward(15)
-
-
-def draw_circle(tilt: int):
-    change_color_to_random()
-    t.right(tilt)
-    t.circle(100)
+extracted_colors = []
 
 get_ready()
 
-# for a in range(2000):
-#     draw_line()
+for color in colorgram.extract("Hirst painting.jpeg", 100):
+    extracted_colors.append((color.rgb.r, color.rgb.g, color.rgb.b))
 
-for _ in range(36):
-    draw_circle(10)
+starting_y = -260
+starting_x = - 230
+t.teleport(starting_x, starting_y)
+
+for i in range(100):
+    if i % 10 == 0:
+        print("kek")
+        starting_y += 50
+        t.teleport(starting_x, starting_y)
+    draw_dot(random.choice(extracted_colors))
+    t.forward(50)
 
 
 screen = turtle.Screen()
