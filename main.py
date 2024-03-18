cda699082af01b97237b7dc231760da1160f351d
DSeratila <dm.seratila@gmail.com>
2024-03-18 22:41:13 +0200

    day 18 circles

--- main.py
+++ main.py
@@ -3,40 +3,52 @@ from turtle import Turtle
 import random
 
 t = Turtle()
-t.shape("turtle")
-t.color('red')
+# t.shape("turtle")
+# t.color('red')
 t.screen.colormode(255)
-t.penup()
-t.left(90)
-t.forward(300)
-t.right(90)
-t.pendown()
 
-for i in range(3, 21):
+
+def get_ready():
+    # t.pensize(7)
+    t.speed(0)
+    # t.penup()
+    # t.left(90)
+    # t.forward(300)
+    # t.right(90)
+    # t.pendown()
+
+
+def change_color_to_random():
     g = random.randrange(0, 257, 10)
     b = random.randrange(0, 257, 10)
     r = random.randrange(0, 257, 10)
-    print(r, g, b)
-    tup = (r, g, b)
-    t.pencolor(tup)
-    angle = 360/i
-    # if angle != 90:
-    t.right(angle)
-    for r in range(0, i):
-        if i % 2 == 0:
-            for _ in range(0, 5):
-                t.forward(10)
-                t.penup()
-                t.forward(4)
-                t.pendown()
-                t.forward(2)
-                t.penup()
-                t.forward(4)
-                t.pendown()
-        else:
-            t.forward(100)
-        if r != i-1:
-            t.right(angle)
+    t.pencolor(r, g, b)
+
+
+def get_random_direction():
+    directions = [270, 90, 0, 180]
+    return random.choice(directions)
+
+
+def draw_line():
+    change_color_to_random()
+    t.setheading(get_random_direction())
+    t.forward(15)
+
+
+def draw_circle(tilt: int):
+    change_color_to_random()
+    t.right(tilt)
+    t.circle(100)
+
+get_ready()
+
+# for a in range(2000):
+#     draw_line()
+
+for _ in range(36):
+    draw_circle(10)
+
 
 screen = turtle.Screen()
 screen.exitonclick()
