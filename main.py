0c99b2d1b1cbdc2ab2cdb3e5f387329f57148027
DSeratila <dm.seratila@gmail.com>
2024-03-27 21:28:30 +0200

    day 19 turtle movements on keystroke

--- main.py
+++ main.py
@@ -1,57 +1,32 @@
 import turtle
-from turtle import Turtle
+from turtle import Turtle, Screen
 import random
-import colorgram
 
 
-t = Turtle()
-# t.shape("turtle")
-# t.color('red')
+tur = Turtle()
+scr = Screen()
 
 
-def get_ready():
-    t.pensize(20)
-    t.screen.colormode(255)
-    t.speed(0)
-    # t.penup()
-    # t.left(90)
-    # t.forward(300)
-    # t.right(90)
-    # t.pendown()
+def move_forwards():
+    tur.forward(10)
+
+def rotate_right():
+    tur.right(10)
+
+def rotate_left():
+    tur.left(10)
+
+def clear():
+    tur.reset()
+
+scr.listen()
+scr.onkey(key="w", fun=move_forwards)
+scr.onkey(key="d", fun=rotate_right)
+scr.onkey(key="a", fun=rotate_left)
+scr.onkey(key="c", fun=clear)
 
 
-def change_color_to_random():
-    g = random.randrange(0, 257, 10)
-    b = random.randrange(0, 257, 10)
-    r = random.randrange(0, 257, 10)
-    t.pencolor(r, g, b)
 
-def draw_dot(color: turtle):
-    t.pendown()
-    t.pencolor(color[0], color[1], color[2])
-    t.forward(1)
-    t.penup()
+scr.exitonclick()
 
-extracted_colors = []
-
-get_ready()
-
-for color in colorgram.extract("Hirst painting.jpeg", 100):
-    extracted_colors.append((color.rgb.r, color.rgb.g, color.rgb.b))
-
-starting_y = -260
-starting_x = - 230
-t.teleport(starting_x, starting_y)
-
-for i in range(100):
-    if i % 10 == 0:
-        print("kek")
-        starting_y += 50
-        t.teleport(starting_x, starting_y)
-    draw_dot(random.choice(extracted_colors))
-    t.forward(50)
-
-
-screen = turtle.Screen()
-screen.exitonclick()
 
