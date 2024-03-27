599fe756775999a33de8aa1efbeb7fd02032feff
DSeratila <dm.seratila@gmail.com>
2024-03-27 22:58:16 +0200

    day 19 turtle race

--- main.py
+++ main.py
@@ -3,27 +3,41 @@ from turtle import Turtle, Screen
 import random
 
 
-tur = Turtle()
+
 scr = Screen()
+scr.setup(500, 400)
+user_bet = scr.textinput(title="Make your bet", prompt="Which turtle is going to win? Enter the color:")
+colors = ["red", "orange", "teal", "green", "blue", "purple"]
+is_race_on = False
+all_turtles = []
+
+for i in range(0, 6):
+    t = Turtle(shape='turtle')
+    t.color(colors[i])
+    all_turtles.append(t)
 
 
-def move_forwards():
-    tur.forward(10)
+starting_x_pos = -240
+starting_y_pos = 90
 
-def rotate_right():
-    tur.right(10)
+for t in all_turtles:
+    t.teleport(starting_x_pos, starting_y_pos)
+    starting_y_pos -= 25
+    t.penup()
 
-def rotate_left():
-    tur.left(10)
 
-def clear():
-    tur.reset()
+if user_bet:
+    is_race_on = True
 
-scr.listen()
-scr.onkey(key="w", fun=move_forwards)
-scr.onkey(key="d", fun=rotate_right)
-scr.onkey(key="a", fun=rotate_left)
-scr.onkey(key="c", fun=clear)
+while is_race_on:
+    if t.xcor() > 230:
+        is_race_on = False
+        print(t.pencolor())
+        if t.pencolor() == user_bet:
+            print("You've won")
+    for t in all_turtles:
+        t.forward(random.randint(0, 10))
+
 
 
 
