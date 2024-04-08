8267e2932265c90e25b5df71ddac93fd1c1633ac
DSeratila <dm.seratila@gmail.com>
2024-04-09 01:06:45 +0300

    day 21 snake game simple

--- main.py
+++ main.py
@@ -1,42 +1,51 @@
 import time
-import turtle
-from turtle import Turtle, Screen
-import random
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+# import random
 
 
 scr = Screen()
 scr.setup(600, 600)
 scr.bgcolor("black")
 scr.title("Snake game")
-#disable refreshing the screen
+# disable refreshing the screen
 scr.tracer(0)
 
-snake = []
-starting_pos = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
+food = Food()
+score = Scoreboard()
+scr.listen()
 
-for i in range(0, 3):
-    t = Turtle()
-    t.color("white")
-    t.shape("square")
-    t.penup()
-    t.teleport(starting_pos[i][0], starting_pos[i][1])
-    snake.append(t)
+scr.onkey(snake.up, "Up")
+scr.onkey(snake.down, "Down")
+scr.onkey(snake.left, "Left")
+scr.onkey(snake.right, "Right")
 
 game_is_on = True
 
 while game_is_on:
     scr.update()
     time.sleep(0.1)
+    snake.move()
 
-    for i in range(len(snake) - 1, 0, -1):
-        # print(i)
-        x = snake[i - 1].xcor()
-        y = snake[i - 1].ycor()
-        snake[i].goto(x, y)
-        # snake[i].setx(x)
-        # snake[i].sety(y)
-    snake[0].forward(20)
-    
+    # food collision check
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    # wall collision
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280:
+        game_is_on = False
+        score.game_over()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+
 scr.exitonclick()
-
-
