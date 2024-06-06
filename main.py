da6a854d83025b661d39ea828621a21fc84bdb8a
DSeratila <dm.seratila@gmail.com>
2024-06-07 02:48:40 +0300

    day 30 error hangling

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-# try:
-#     file = open("a_file.txt")
-#     a_dictionary = {"key": "value"}
-#     print(a_dictionary["key"])
-# except FileNotFoundError:
-#     file = open("a_file.txt", "2")
-#     file.writelines("something")
-# except KeyError as error_message:
-#     print(f"The Key {error_message} is not found")
-# else:
-#     content = file.read()
-#     print(content)
-# finally:
-#     file.close()
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "2")
+    file.writelines("something")
+except KeyError as error_message:
+    print(f"The Key {error_message} is not found")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+
+
 
