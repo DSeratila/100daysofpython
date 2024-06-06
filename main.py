4d1de028b68518b658ed33621016841e09eea322
DSeratila <dm.seratila@gmail.com>
2024-06-07 02:33:08 +0300

    day 30 exceptions 1

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import tkinter
-
-# window = tkinter.Tk()
-# window.title("Example.com")
-# window.minsize(500, 300)
-# window.mainloop()
-#
-#
-# label = tkinter.Label(text="Label example", font=("Arial", 24, "bold"))
-# label.pack(side="left")
-
-# advanced arguments
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "2")
+#     file.writelines("something")
+# except KeyError as error_message:
+#     print(f"The Key {error_message} is not found")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
 
